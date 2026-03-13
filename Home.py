4e1882f3caca7660e6ced6239d52e9205d09c9e0
Nuril Hidayati <nurilassajdah@gmail.com>
2026-03-13 09:07:35 +0700

    update

--- Home.py
+++ Home.py
@@ -9,4 +9,10 @@ st.header("1. Draw Polygon")
 st.write("Geohash can be obtained by draw polygon over area you want to obtain geohash, then pick digit geohash you want, after that another map will popup show geohash coverage based your polygon drawing. ")
 
 
+st.header("2. Geohash Converter")
+st.write("This tool is for converting geohash to polygon or polygon to geohash. You can upload geohash file or polygon file and convert it to other format. And also you can convert area to geohash and geohash to csv.")
+
+st.header("3. Copy Coordinates")
+st.write(" If you have list of coordinates separated by comma, you can just paste it in fill column and the choose which digit geohash you want then you can download it as csv")
+
 
